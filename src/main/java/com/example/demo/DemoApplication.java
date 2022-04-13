82a4e1f13b95650c02be69dd80e9afebf3989e6b
shuhaoyun <101231628+shuhaoyun@users.noreply.github.com>
2022-04-13 23:07:43 +0800

    tesett

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//nihao
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
